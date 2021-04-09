6be4a65119cf317c8528e69901befcba78e6d2a0
petrochvat <petrochvat@gmail.com>
2021-04-09 22:22:35 +0200

    smaller refactoring

--- build/index.js
+++ build/index.js
@@ -1,45 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var perf_hooks_1 = require("perf_hooks");
-var prompt = require('prompt');
 var readlineSync = require('readline-sync');
 var fs = require('fs');
-var ReverseWords = /** @class */ (function () {
-    function ReverseWords() {
-        this.word = readlineSync.question('Write a word and I do magic with that? ', { limit: /^[a-zA-Z0-9_.-]*$/ });
-    }
-    ReverseWords.prototype.reverseString = function () {
-        var newString = "";
-        for (var i = this.word.length - 1; i >= 0; i--) {
-            newString += this.word[i];
-        }
-        return newString;
-    };
-    ReverseWords.prototype.changeUpperToLowerAndViceVersa = function (str) {
-        return str.replace(/([a-z]+)|([A-Z]+)/g, function (match, chr) {
-            return chr ? match.toUpperCase() : match.toLowerCase();
-        });
-    };
-    ReverseWords.createJsonFile = function (pathToJson, dict) {
-        var data = JSON.stringify(dict, null, 2);
-        fs.writeFileSync(pathToJson, data, function (err) {
-            if (err) {
-                console.log('Error writing file', err);
-            }
-            else {
-                console.log('Successfully wrote file');
-            }
-        });
-    };
-    return ReverseWords;
-}());
-;
+var reverseWords_1 = require("./reverseWords");
 var t0 = perf_hooks_1.performance.now();
-var greeter = new ReverseWords();
+var greeter = new reverseWords_1.ReverseWords();
 var avs = greeter.reverseString();
 var ab = greeter.changeUpperToLowerAndViceVersa(avs);
 var t1 = perf_hooks_1.performance.now();
 var dict = { "input": greeter.word,
     "output": ab,
     "performance": (t1 - t0) };
-ReverseWords.createJsonFile('processed.json', dict);
+reverseWords_1.ReverseWords.createJsonFile('processed.json', dict);
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,10 @@
 import { performance } from "perf_hooks";
 
-const prompt = require('prompt');
 const readlineSync = require('readline-sync');
 const fs = require('fs');
+import { ReverseWords } from "./reverseWords";
 
 
-class ReverseWords {
-    word: string;
-    constructor() {
-        this.word = readlineSync.question('Write a word and I do magic with that? ', {limit: /^[a-zA-Z0-9_.-]*$/})
-    }
-
-    reverseString() {
-        let newString = "";
-    for (let i = this.word.length - 1; i >= 0; i--) {
-        newString += this.word[i];
-    }
-    return newString;
-    }
-
-    changeUpperToLowerAndViceVersa(str: string) {
-        return str.replace(/([a-z]+)|([A-Z]+)/g, function(match, chr) {
-            return chr ? match.toUpperCase() : match.toLowerCase();
-        });
-    }
-
-    static createJsonFile(pathToJson: string, dict: object) {
-        let data = JSON.stringify(dict, null, 2);
-        fs.writeFileSync(pathToJson, data, (err: any) => {
-            if (err) {
-                console.log('Error writing file', err)
-            } else {
-                console.log('Successfully wrote file')
-            }
-        }
-        )}
-
-};
 let t0 = performance.now();
 let greeter = new ReverseWords();
 let avs = greeter.reverseString();
